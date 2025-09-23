2957972ed0c656598ff7dd35c19b00a47a0611aa
kelly26256 <kelly.26256@students.kyu.ac.ke>
2025-09-23 14:46:43 +0300

    Add files via upload

---
+++ c. basics.c
@@ -0,0 +1,23 @@
+//simple c program
+/*Name:Kelly Mwiti Muriuki
+    Reg : CT100/G/26256/25
+    Description: Hello.c
+    */
+    
+    #include <stdio.h>
+    
+//main function
+   int main () {
+   	printf ( "I'm kelly a student in KYU pursuing a degree in bachelor of science in Information Technology(Bsc IT). \n");
+   	printf ( "I'm taking 8 units this semester. \n");
+   	printf("They are as follows: \n");
+   	printf("1. Mathematics for science (SPM 2104). \n");
+   	printf("2. Introduction to programming and Alogorithms (SPI 2101). \n");
+   	printf("3. Communication skills (UCU 2101).    \n");
+   	printf("4. Essentials of Accounting (SPI 2120). \n");
+   	printf("5. Introduction to computer applications (SPI 2103).\n");
+   	printf("6. Fundamentals of ICT (UCU 2105). \n");
+   	printf("7. Computer system and organization (SPI 2101). \n");
+   	printf("8. Analogue electronics (SPH 2107). \n");
+   	return 0;
+   }
