6fd51f35f7aa74083364d850fb77c7afe4709b60
kelly26256 <kelly.26256@students.kyu.ac.ke>
2025-10-08 15:00:45 +0300

    Update for_loop.c

--- for_loop.cpp
+++ for_loop.cpp
@@ -1,11 +1,18 @@
+/*
+NAME:Kelly Mwiti Muriuki
+REG.NO:CT100/G/26256/25
+DESCRIPTION:printing numbers 100 to 50
+*/
+
 #include <stdio.h>
 
 int main(){
 	int num;
-	
+	//check condition and excecute
 	for(num=100; num>=50; --num){
 		printf("%d\n", num);
 	}
 		
 	return 0;
+
 }
