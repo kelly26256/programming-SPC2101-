83e18df703d213c48aac7ab56ef6e5ea6af94495
kelly26256 <kelly.26256@students.kyu.ac.ke>
2025-10-08 14:54:34 +0300

    Update password.c

--- password.c
+++ password.c
@@ -1,11 +1,17 @@
+/*
+NAME:Kelly Mwiti Muriuki 
+REG. NO:CT100/G/26256/25
+DESCRIPTION:checking password
+*/
+
 #include <stdio.h>
 #include <string.h>
 
 int main(){
-	char password[20];
+	char password[20];//array to store password
 	
 	do {
-		printf("Enter the password:\t");
+		printf("Enter the password:\t");//prompt user to enter password
 		scanf("%s", &password);
 		
 		if(strcmp(password, "1234") != 0){
@@ -20,4 +26,5 @@ int main(){
 	
 	
 	return 0;
+
 }
